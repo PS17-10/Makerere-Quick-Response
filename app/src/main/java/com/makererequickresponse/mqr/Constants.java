efef2e8977decfa3bb7739580bc9e57b8b79c011
StephenAraka <stevenaraka@gmail.com>
2017-08-20 16:07:21 -0700

    adding firebase push notifications

---
+++ app/src/main/java/com/makererequickresponse/mqr/Constants.java
@@ -0,0 +1,23 @@
+package com.makererequickresponse.mqr;
+
+/**
+ * Created by Belal on 3/20/2016.
+ */
+public class Constants {
+
+    //Firebase app url
+    public static final String FIREBASE_APP = "https://makerere-quick-response.firebaseio.com/";
+
+    //Constant to store shared preferences
+    public static final String SHARED_PREF = "notificationapp";
+
+    //To store boolean in shared preferences for if the device is registered to not
+    public static final String REGISTERED = "registered";
+
+    //To store the firebase id in shared preferences
+    public static final String UNIQUE_ID = "uniqueid";
+
+    //register.php address in your server
+    public static final String REGISTER_URL = "http://localhost/mqr/register.php";
+
+}
---
+++ app/src/main/java/com/makererequickresponse/mqr/MyApplication.java
@@ -0,0 +1,19 @@
+package com.makererequickresponse.mqr;
+
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Belal on 3/20/2016.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Initializing firebase
+        Firebase.setAndroidContext(getApplicationContext());
+    }
+}
