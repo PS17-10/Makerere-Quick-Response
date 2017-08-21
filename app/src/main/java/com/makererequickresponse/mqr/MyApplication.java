b106133c26773ee634d791c78be064a12ae1ffed
StephenAraka <stevenaraka@gmail.com>
2017-08-21 09:46:19 -0700

    setup notification service

--- app/src/main/java/com/makererequickresponse/mqr/MyApplication.java
+++ app/src/main/java/com/makererequickresponse/mqr/MyApplication.java
@@ -14,6 +14,6 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         //Initializing firebase
-        Firebase.setAndroidContext(getApplicationContext())
+        Firebase.setAndroidContext(getApplicationContext());
     }
 }
